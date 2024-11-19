77cd615df8a64ed2764271372a808051e2ce5d05
StingStorm <alexkalganov18@gmail.com>
2024-11-19 03:12:44 +0200

    remove unnecessary types

--- src/App.types.ts
+++ src/App.types.ts
@@ -1,36 +1,3 @@
-type Nullable<T> = T | null;
-
-interface VeryBasic {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  urls: {
-    full: string;
-    raw: string;
-    regular: string;
-    small: string;
-    thumb: string;
-  };
-}
-
-interface Basic extends VeryBasic {
-  alt_description: Nullable<string>;
-  blur_hash: Nullable<string>;
-  color: Nullable<string>;
-  description: Nullable<string>;
-  height: number;
-  likes: number;
-  links: {
-    self: string;
-    html: string;
-    download: string;
-    download_location: string;
-  };
-  promoted_at: Nullable<string>;
-  width: number;
-  user: unknown;
-}
-
 export type GalleryItemType = {
   id: string;
   urls: {
@@ -40,6 +7,6 @@ export type GalleryItemType = {
     small?: string;
     thumb?: string;
   };
-  alt_description: Nullable<string>;
+  alt_description: string | null;
   likes: number;
 };
